a1a91fd9caa7341794f5eab5db7e51f3710ef693
VitalBel1981 <Va06072007!>
2021-02-15 12:27:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+me = {}
+me["married"] = ["yes"]
+print(me)
+me["phone_number"] = [80296251218]
+print(me)
+del me["phone_number"]
+print(me)
+me["car"] = ["opel zafira"]
+print(me)
+me["car"] = ["Lamborgini"]
+print(me)
---
+++ music.py
@@ -0,0 +1,21 @@
+user1 = {}
+user1["User_name"] = ["Anton"]
+user1["is_active"] = ["true"]
+user1["Bands"] = ["ДДТ", "Ляпис", "Nirvana", "Океан Эльзы", "Хорошки"]
+user1["favorite_songs"] = ["Здраствуй чужая милая"]
+print(user1)
+user2 = {}
+user2["User_name"] = ["Volodiya"]
+user2["is_active"] = ["true"]
+user2["Bands"] = ["Beatles", "Газманов", "Красная плесень", "Океан Эльзы"]
+user2["favorite_songs"] = ["Yesterday"]
+print(user2)
+user3 = {}
+user3["User_name"] = ["Sancho"]
+user3["is_active"] = ["true"]
+user3["Bands"] = ["Виагра", "Сектор газа", "Дай дорогу", "Песняры"]
+user3["favorite_songs"] = ["А на войне как на войне"]
+songs = [{"name": "Касиу Ясь Канюшыну", "author": "х.з"}, {"name": "Когда я на почте служил ямщиком", "author": "Агата Кристи"}]
+print(songs)
+users = [user1, user2, user3]
+print(users)
